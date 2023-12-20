b4e41c7a48d3a705c8d5fb2009a659e18ae53066
ktellawi <koutaiba@msn.com>
2023-12-20 19:12:55 +0300

    first

---
+++ tailwind.config.ts
@@ -0,0 +1,50 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        primaryOne:"#0F9887",
+        primarTwo:"#D31152",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+    'animation': {
+      'text':'text 5s ease infinite',
+  },
+  'keyframes': {
+      'text': {
+          '0%, 100%': {
+             'background-size':'200% 200%',
+              'background-position': 'left center'
+          },
+          '50%': {
+             'background-size':'200% 200%',
+              'background-position': 'right center'
+          }
+      },
+  }
+
+  },
+  plugins: [],
+}
+export default config
