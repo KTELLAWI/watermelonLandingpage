cdb1ac8642a6e84a68686e9827a136950083dde4
KTELLAWI <koutaiba@msn.com>
2024-01-07 22:40:06 +0000

    hhfhy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
+
         domains: ['i.imgur.com', '*'],// Allow loading images from any domain
       },
     output: 'export',
